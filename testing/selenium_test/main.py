baec91dfa5bcdb4509feafef23261a42f3855468
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2025-03-07 21:19:58 +0800

    fix chrome driver

--- testing/selenium_test/main.py
+++ testing/selenium_test/main.py
@@ -3,6 +3,7 @@ import platform
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import test_title
 
 def setup_driver():
@@ -12,18 +13,20 @@ def setup_driver():
     if system_name == "Windows":
         # Windows 環境：設定 Windows 專用的參數
         chrome_options.add_argument("--user-data-dir=C:\\temp\\unique_user_data_dir")
-        # 如有其他 Windows 特定設定，可在此加入
+        
+        service = Service()  
+        driver = webdriver.Chrome(service=service, options=chrome_options)
     elif system_name == "Linux":
         # Ubuntu/CI 環境：設定 headless 模式與相關參數
         chrome_options.add_argument("--user-data-dir=/tmp/unique_user_data_dir")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     else:
         raise Exception("不支援的作業系統: " + system_name)
 
-    service = Service()  
-    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.get("http://localhost:5173/")
     return driver
 
