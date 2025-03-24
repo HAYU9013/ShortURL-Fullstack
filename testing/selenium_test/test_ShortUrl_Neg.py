7c9079de0baccc50674fd066afb9421dfe8cb911
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2025-03-24 21:06:11 +0800

    tmp

--- testing/selenium_test/test_ShortUrl_Neg.py
+++ testing/selenium_test/test_ShortUrl_Neg.py
@@ -15,6 +15,7 @@ def test_shortUrl_Neg(driver):
             time.sleep(2)
             wait = WebDriverWait(driver, timeout=2)
             error_message = inputField.get_attribute("validationMessage")
+            print(error_message)
             assert error_message == "請輸入網址。" or error_message == "請填寫這個欄位。"
             inputField.clear()
             
